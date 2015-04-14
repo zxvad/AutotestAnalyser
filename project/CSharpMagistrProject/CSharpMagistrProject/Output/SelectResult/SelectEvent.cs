ab572a12b5c3f0c1ba4606b2f572d8e7720949b2
KABAL64 <KABAL64@yandex.ru>
2015-04-14 23:17:11 +0400

    SelectEvent.cs, SelectNeedEvent.cs

--- project/CSharpMagistrProject/CSharpMagistrProject/Output/SelectResult/SelectEvent.cs
+++ project/CSharpMagistrProject/CSharpMagistrProject/Output/SelectResult/SelectEvent.cs
@@ -1,9 +1,36 @@
 ﻿
 
+using System.Windows.Forms;
+using CSharpMagistrProject.DB;
+
 namespace CSharpMagistrProject.Output.SelectResult
 {
-    class SelectEvent
+    public class SelectEvent
     {
-       public void Show(){}
+        private string sourceEventTable;
+        public string SourceEventTable
+        {
+            get { return sourceEventTable; }
+        }
+
+        private DataBase dataBase;
+        public DataBase DataBase
+        {
+            get { return dataBase; }
+        }
+
+        public SelectEvent(DataBase sourceDataBase, string sourceEventTable)
+        {
+            this.sourceEventTable = sourceEventTable;
+            dataBase = sourceDataBase;
+            dataBase.Connect();
+        }
+
+        //Вывод списка событий
+        public void Show(DataGridView receiverGridView)
+        {
+            string queryText = "SELECT * FROM " + sourceEventTable;
+            receiverGridView.DataSource = dataBase.DoSelectQuery(queryText);
+        }
     }
 }
--- project/CSharpMagistrProject/CSharpMagistrProject/Output/SelectResult/SelectNeedEvent.cs
+++ project/CSharpMagistrProject/CSharpMagistrProject/Output/SelectResult/SelectNeedEvent.cs
@@ -1,9 +1,32 @@
 ﻿
 
+using System.Windows.Forms;
+using CSharpMagistrProject.DB;
+
 namespace CSharpMagistrProject.Output.SelectResult
 {
-    class SelectNeedEvent
+    public class SelectNeedEvent
     {
-        public void Show(){}
+        private DataBase dataBase;
+        private string sourceEventTable;
+        private string sourceNeedEventTable;
+
+        public SelectNeedEvent(DataBase sourceDataBase, string sourceEventTable, string sourceNeedEventTable)
+        {
+            dataBase = sourceDataBase;
+            this.sourceEventTable = sourceEventTable;
+            this.sourceNeedEventTable = sourceNeedEventTable;
+        }
+
+
+        //Показ всех необходимых событий
+        public void Show(DataGridView receiverGridView)
+        {
+            string queryText = "SELECT NeedEvent.id, Event.name " +
+                               "FROM (" + sourceNeedEventTable + " NeedEvent " +
+                               "LEFT JOIN " + sourceEventTable + " Event " +
+                               "ON NeedEvent.idEvent = Event.id)";
+            receiverGridView.DataSource = dataBase.DoSelectQuery(queryText);
+        }
     }
 }
