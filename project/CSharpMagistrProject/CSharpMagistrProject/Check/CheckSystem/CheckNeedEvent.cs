b12bf4670f779534322dd25322d6bfb1945e0f79
KABAL64 <KABAL64@yandex.ru>
2015-04-14 23:18:01 +0400

    CheckNeedEvent.cs

--- project/CSharpMagistrProject/CSharpMagistrProject/Check/CheckSystem/CheckNeedEvent.cs
+++ project/CSharpMagistrProject/CSharpMagistrProject/Check/CheckSystem/CheckNeedEvent.cs
@@ -1,10 +1,38 @@
 ﻿
 
+using System.Collections.Generic;
+using CSharpMagistrProject.DB;
+
 namespace CSharpMagistrProject.Check.CheckSystem
 {
-    class CheckNeedEvent
+    public class CheckNeedEvent
     {
+        private DataBase dataBase;
+        private string sourceNeedEventTable;
+
+        public CheckNeedEvent(DataBase sourceDataBase, string sourceNeedEventTable)
+        {
+            dataBase = sourceDataBase;
+            this.sourceNeedEventTable = sourceNeedEventTable;
+        }
+
+
+
         public void CheckForCorrect(){}
-        public void CheckForUnique(){}
+
+
+        public bool CheckForUnique(int idEvent)
+        {
+            string queryText = "SELECT COUNT(*) " +
+                               "FROM " + sourceNeedEventTable +
+                               " WHERE idEvent=?";
+
+            Dictionary<string, object> parametrsDictionary = new Dictionary<string, object>();
+            parametrsDictionary.Add("idEvent", idEvent);
+
+            if (dataBase.DoScalarQuery(queryText, parametrsDictionary) > 0)
+                return false;
+            return true;
+        }
     }
 }
